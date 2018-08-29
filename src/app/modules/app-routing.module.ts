6dc1d4cda9e365f9fcc4f0969346ee6ca819115d
Alaude30 <kuramamode11@gmail.com>
2018-08-29 21:14:04 +0800

    add post route

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { HomeComponent } from '../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
+import { PostDisplayComponent } from '../components/posts/post-display/post-display.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'post/:id', component: PostDisplayComponent }
 ]
 
 @NgModule({
