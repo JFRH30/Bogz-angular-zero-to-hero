906382f7da819b101af8030e256a6df7dd918bda
Alaude30 <kuramamode11@gmail.com>
2018-08-29 19:27:24 +0800

    add some http request to local json server

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { PostModel } from '../models/post.model';
 
 const httpOptions = {
     headers: new HttpHeaders ({
@@ -12,7 +15,17 @@ const httpOptions = {
 })
 
 export class PostsService {
+    postsUrl: string = 'http://localhost:3000/posts';
+
     constructor (
         private http: HttpClient
     ) { }
+
+    getPosts (): Observable<PostModel[]> {
+        return this.http.get<PostModel[]>( this.postsUrl )
+    }
+
+    getPostsWithUser (): Observable<PostModel[]> {
+        return this.http.get<PostModel[]>(`${this.postsUrl}?_embed=comments`)
+    }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { UserModel } from '../models/user.model';
 
 const httpOptions = {
     headers: new HttpHeaders ({
@@ -12,7 +15,17 @@ const httpOptions = {
 })
 
 export class UsersService {
+    usersUrl: string = 'http://localhost:3000/users';
+
     constructor (
         private http: HttpClient
     ) { }
+
+    getUsers (): Observable<UserModel[]> {
+        return this.http.get<UserModel[]>(this.usersUrl)
+    }
+
+    getUsersWithPost (): Observable<UserModel[]> {
+        return this.http.get<UserModel[]>( `${this.usersUrl}?_embed=posts` )
+    }
 }
